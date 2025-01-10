f350439b6a4f2063616b7e50ddd6e5318d2c135a
EmuTheGreat <thegreatfrut@mail.ru>
2025-01-10 18:36:38 +0500

    -

--- Assets/Scripts/LevelEditor.cs
+++ Assets/Scripts/LevelEditor.cs
@@ -17,6 +17,8 @@ public class LevelEditor : MonoBehaviour
 
     public GameObject[] tiles;
 
+    public GameObject[] surfacesTiles;
+
     void OnValidate()
     {
         if (levelLayout == null || levelLayout.Length != gridWidth * gridHeight)
@@ -35,6 +37,18 @@ public class LevelEditor : MonoBehaviour
             {
                 Vector3 position = GetPositionFromGrid(x, y);
                 GameObject prefab = levelLayout[GetArrayIndex(x, y)];
+
+                if (prefab != null && prefab.name.StartsWith("Tile"))
+                {
+                    if (levelLayout[GetArrayIndex(x, y + 1)] == null)
+                    {
+                        prefab = surfacesTiles[Random.Range(0, surfacesTiles.Length)];
+                    }
+                    else
+                    {
+                        prefab = tiles[Random.Range(0, tiles.Length)];
+                    }
+                }
 
                 if (prefab != null)
                 {
@@ -123,12 +137,10 @@ public class LevelEditor : MonoBehaviour
             for (int y = -1; y > -6; y--)
             {
                 Vector3 position = GetPositionFromGrid(x, y);
-                GameObject prefab = tiles[0];
+                GameObject prefab = prefab = tiles[Random.Range(0, tiles.Length)];
 
-                if (prefab != null)
-                {
-                    InstantiateTile(prefab, position);
-                }
+                InstantiateTile(prefab, position);
+
             }
         }
     }
